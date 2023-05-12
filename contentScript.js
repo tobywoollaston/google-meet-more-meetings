341f561f20c1c72bbbb24ad4c0b5a0b42f211cca
George Harris <citizenharris@gmail.com>
2023-05-12 13:55:48 +0100

    Add button role

--- contentScript.js
+++ contentScript.js
@@ -1,27 +1,26 @@
 (() => {
-
     chrome.runtime.onMessage.addListener((obj, sender, response) => {
         const { type, token } = obj;
 
         if (type === "NEW") {
             if (isMeetHomePage()) {
-                console.log('home page');
+                console.log("home page");
                 addMeetings(token.token);
             } else {
-                console.log('not home page');
+                console.log("not home page");
             }
         }
     });
 
     const isMeetHomePage = () => {
-        const newMeetingText = "New meeting"
-        let buttons = Array.from(document.getElementsByTagName("button"))
-        let button = buttons.find(x => x.innerHTML?.includes(newMeetingText));
+        const newMeetingText = "New meeting";
+        let buttons = Array.from(document.getElementsByTagName("button"));
+        let button = buttons.find((x) => x.innerHTML?.includes(newMeetingText));
 
         if (button) {
-            return true
+            return true;
         } else {
-            return false
+            return false;
         }
     };
 
@@ -29,56 +28,63 @@
         meetingsDiv.style.width = "100%";
         meetingsDiv.style.borderCollapse = "collapse";
 
-        meetings.forEach(meet => {
-            let meetingRow = document.createElement('tr');
+        meetings.forEach((meet) => {
+            let meetingRow = document.createElement("tr");
+            meetingRow.setAttribute("role", "button");
             meetingRow.style.height = "65px";
             meetingRow.style.borderStyle = "solid";
             meetingRow.style.borderWidth = "1px 0";
             meetingRow.style.borderColor = "rgba(32,33,36,.2)";
-            meetingRow.addEventListener('click', () => {
+            meetingRow.addEventListener("click", () => {
                 window.location.replace(meet.meet);
             });
 
-            let dateColumn = document.createElement('td');
+            let dateColumn = document.createElement("td");
             dateColumn.innerHTML = getDisplayTimeFor(meet);
             dateColumn.style.width = "112px";
             dateColumn.style.color = "rgba(32,33,36,.67)";
             dateColumn.style.fontSize = "16px";
 
-            let nameColumn = document.createElement('td');
+            let nameColumn = document.createElement("td");
             nameColumn.innerHTML = meet.name;
             nameColumn.style.textAlign = "left";
             nameColumn.style.fontSize = "18px";
-            
+
             meetingRow.appendChild(dateColumn);
             meetingRow.appendChild(nameColumn);
             meetingsDiv.appendChild(meetingRow);
         });
-    }
+    };
 
     const addMeetings = async (token) => {
         const xpath = "//div[contains(text(), 'From your Google Calendar')]";
-        const sibling = document.evaluate(xpath, document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue;
+        const sibling = document.evaluate(
+            xpath,
+            document,
+            null,
+            XPathResult.FIRST_ORDERED_NODE_TYPE,
+            null
+        ).singleNodeValue;
 
-        const meetingsBlock = sibling.previousSibling//document.getElementsByClassName("VdLOD yUoCvf JxfZTd")[0];
+        const meetingsBlock = sibling.previousSibling; //document.getElementsByClassName("VdLOD yUoCvf JxfZTd")[0];
         meetingsBlock.innerHTML = "";
-        let meetingTable = document.createElement('table');
+        let meetingTable = document.createElement("table");
         meetingsBlock.appendChild(meetingTable);
 
         let meetings = await getGoogleMeets(token);
         let allDayEvents = getAllDayMeetings(meetings);
         let timedEvents = getSlotMeetings(meetings);
-    
+
         displayMeetings(allDayEvents, meetingTable);
         displayMeetings(timedEvents, meetingTable);
 
         meetingTable.firstChild.style.borderTop = "none";
         meetingTable.lastChild.style.borderBottom = "none";
 
-        var css = 'table tr:hover { background-color: rgb(26,115,232); color: white !important; }';
-        let style = document.createElement('style');
+        var css =
+            "table tr:hover { background-color: rgb(26,115,232); color: white !important; }";
+        let style = document.createElement("style");
         style.appendChild(document.createTextNode(css));
         meetingTable.appendChild(style);
     };
 })();
-
